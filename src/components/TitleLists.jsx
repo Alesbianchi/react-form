ab8c7d7c126cf7f8993711d09911d4c9fd5d5882
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-02-18 16:00:09 +0100

    add title test in log

--- src/components/TitleLists.jsx
+++ src/components/TitleLists.jsx
@@ -9,12 +9,19 @@ export default function TitleLists() {
     const [tasks, setTasks] = useState(initialTasks);
     const [newTask, setNewTask] = useState('');
 
+    const addTask = e => {
+        e.preventDefault();
+        const updateTasks = [...tasks, newTask];
+        setTasks(updatedTasks);
+    }
+
+
     return (
         <div>
             <h2>Lista Titoli</h2>
             <ul>
-                {tasks.map((task, index) => (
-                    <li key={index}>{task}</li>
+                {tasks.map((task, i) => (
+                    <li key={i}>{task}</li>
                 ))}
             </ul>
         </div>
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -3,8 +3,20 @@ import { useState } from 'react';
 export default function UserInput() {
 
     const [firstTitle, setFirstTitle] = useState('titolo');
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log('titolo inviato è:' + firstTitle);
+
+    }
 
     return (
-        <input type="text" value={firstTitle} />
+        <>
+            <form onSubmit={handleSubmit} >
+                <input type="text" value={firstTitle}
+                    onChange={e => { setFirstTitle(e.target.value) }}
+                />
+                <button>Invia</button>
+            </form>
+        </>
     )
 }
